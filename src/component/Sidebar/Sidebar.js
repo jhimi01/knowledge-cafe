77c2ec74350798b27aba0c74cbf8945fe6f71b27
Jhimi Talukder <talukderjhimi@gmail.com>
2023-03-31 10:11:54 +0600

    more css

--- src/component/Sidebar/Sidebar.js
+++ src/component/Sidebar/Sidebar.js
@@ -3,11 +3,11 @@ import React from "react";
 const Sidebar = () => {
   return (
     <>
-      <div className="">
-        <h4 className="p-3 rounded border">spent time on read : 177 min</h4>
+      <div style={{background:'#a29bfe6b', border:'1px solid #6c5ce7'}} className="rounded mb-3">
+        <h4 style={{color:'#6c5ce7'}} className="p-3 rounded">spent time on read : 177 min</h4>
       </div>
 
-      <div className="bg-light p-3 text-start">
+      <div className="bg-light rounded p-3 text-start">
         <h3>Bookmarked Blogs : 5</h3>
         <h5 className="bg-white rounded p-2 mb-4">
           Lorem, ipsum Lorem ipsum dolor sit.
