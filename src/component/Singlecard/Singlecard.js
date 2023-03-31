12aca04d6794387b7fde15fe343d72ab7dbf2bb0
Jhimi Talukder <talukderjhimi@gmail.com>
2023-04-01 01:42:49 +0600

    color book mark

--- src/component/Singlecard/Singlecard.js
+++ src/component/Singlecard/Singlecard.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Singlecard = ({card, addwatchTime, bookmarks}) => {
     const {url, user, user_img, date, details, time} = card;
+
+    const [colorbook, setColorbook] = useState(false);
+const handleboolmarl =()=>{
+  setColorbook(true)
+}
+
     return (
         <div>
             <div className=" mb-4 w-full">
@@ -19,7 +25,7 @@ const Singlecard = ({card, addwatchTime, bookmarks}) => {
 
   <div className='text-secondary d-flex gap-3'>
     <p>{time} min read</p>
-    <p onClick={()=>{bookmarks(card)}} style={{width:'20px', cursor:'pointer'}}>{<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-1 h-1">
+    <p onClick={()=>{bookmarks(card)}} style={{width:'20px', cursor:'pointer'}}>{<svg onClick={handleboolmarl} xmlns="http://www.w3.org/2000/svg" fill={colorbook? 'black': 'none'} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-1 h-1">
   <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
 </svg>}
 </p>
